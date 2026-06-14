b27787605c6b7a4d42c99177ad67b7cfc32a85af
Cristel Cortina Rivera <cristel@cristel.local>
2026-06-13 21:41:45 -0600

    fix laravel cache docker

--- docker/start.sh
+++ docker/start.sh
@@ -8,13 +8,22 @@ if [ "$DB_CONNECTION" = "sqlite" ]; then
     touch "${DB_DATABASE:-database/database.sqlite}"
 fi
 
-# SI NO EXISTE APP_KEY → error controlado
+# APP_KEY obligatorio
 if [ -z "$APP_KEY" ]; then
     echo "ERROR: APP_KEY no configurado (Render o docker -e)"
     exit 1
 fi
 
+# limpiar cache ANTES
+php artisan view:clear
+php artisan config:clear
+php artisan cache:clear
+
+# migraciones
 php artisan migrate --force
+
+# volver a cachear limpio
 php artisan config:cache
 
+# levantar server
 php artisan serve --host=0.0.0.0 --port="${PORT:-8080}"
