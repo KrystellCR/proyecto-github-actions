f7a3ca46b4a4bd928d432909b367bdd7e54b7d8e
Cristel Cortina Rivera <cristel@cristel.local>
2026-06-13 21:33:29 -0600

    test

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 // apiResource crea rutas REST: index, store, update y destroy.
 Route::apiResource('tasks', TaskController::class)
+
     ->only(['index', 'store', 'update', 'destroy']);
